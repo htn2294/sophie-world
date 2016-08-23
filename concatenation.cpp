070b2b59ca0aaf2d74da1b24ea54979ea0893495
htn2294 <htn2294@email.vccs.edu>
2016-08-22 21:14:19 -0400

    Add files via upload

---
+++ concatenation.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string statement1 = "Hello World,";
+    string statement2 = " I am Sophie!";
+    string message = statement1 + statement2;
+
+    cout << message;
+
+    return 0;
+}
