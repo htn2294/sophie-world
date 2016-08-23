724c7b82cad3d8c04658d63af16ad16086533427
htn2294 <htn2294@email.vccs.edu>
2016-08-22 21:04:33 -0400

    added .cpp file

---
+++ Untitled1.cpp
@@ -0,0 +1,22 @@
+// Hoa Nguyen
+// A program that print out the statement "Hello World!"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool flag = true;
+
+int main()
+{
+    string words = "Hello World!";
+
+    if (flag == true)
+    {
+        cout << words << endl;
+
+    }
+
+    return 0;
+}
